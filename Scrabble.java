6e9d180024ca954d83c74e470f750651f412fc89
Caroline Genzel <carolinegenzel@MacBook-Pro-sl-Caroline.local>
2024-12-11 13:39:49 +0200

    done

--- Scrabble.java
+++ Scrabble.java
@@ -88,19 +88,10 @@ public class Scrabble {
 	// (these two vowels make it easier for the user to construct words)
 	public static String createHand() {
 		int length=HAND_SIZE-2;
-		String word= DICTIONARY[(int)(Math.random()*NUM_OF_WORDS)];
-
-		while( word==null && length != word.length()){
-			word= DICTIONARY[(int)(Math.random()*NUM_OF_WORDS)];
-		}
-
-			int random1=(int)(Math.random()*(word.length()));
-			int random2=(int)(Math.random()*(word.length()));
-	
-			word=word.substring(0,random1+1)+"e"+word.substring(random1+1);
-			word=word.substring(0,random2+1)+"a"+word.substring(random2+1);
+		String word= MyString.randomStringOfLetters(length);
+			word=MyString.insertRandomly('a', word);
+			word=MyString.insertRandomly('e', word);
 			
-
 		return word;
 	}
 	
